c20947642f78b9247b43af5406e161e0e86b27b5
Wallace Rossini <rossiniwallace@gmail.com>
2022-12-07 00:16:12 -0300

    feat: add .env

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Orders } from './components/Orders';
 import { GlobalStyles } from './styles/GlobalStyles';
 
 export function App() {
+
   return (
     <>
       <GlobalStyles/>
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL:'http://127.0.0.1:4000/'
+  baseURL: process.env.BASE_API
 });
